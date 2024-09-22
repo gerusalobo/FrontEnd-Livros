63685c874cffe6d8d312003bc62b07cc6563fb6c
gerusalobo <gerusalobo@gmail.com>
2024-09-22 11:26:03 -0300

    update

--- src/index.js
+++ src/index.js
@@ -4,11 +4,7 @@ import './index.css';
 import App from './App';
 
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-
-    <App />,
-    document.getElementById('root')
-
-);
+const container = document.getElementById('root');
+const root = ReactDOM.createRoot(container);
+root.render(<App />);
 
